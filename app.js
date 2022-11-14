cf0c22b003b278c4397b61fdb908b013c5228568
angcam59 <guerra.angelo@orange.fr>
2022-11-14 11:41:02 +0100

    modif page contact angelo

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
+
+app.get('/', (req, res) => {
+    res.render('accueil.ejs')
+})
 app.get('/img', (req, res) => {
     res.render('accueil.ejs')
 })
@@ -20,5 +25,10 @@ app.get('/services', (req, res) => {
     res.render('services.ejs')
 })
 
+app.post("/", (request, response) => {
+
+    response.send("Tu te nommes : " + request.body.lastName +  " " + request.body.firstName +
+    "<br>" + "Ton message : " + request.body.description);
+})
 
     app.listen(3002)
